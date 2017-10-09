081c4d89119f871013fc190137d614d8dbb5326c
JonghwanMun <choco1916@postech.ac.kr>
2017-10-09 17:49:31 +0900

    Update run_mario.sh

--- dataset/playingMario/run_mario.sh
+++ dataset/playingMario/run_mario.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-# create save folder if not exist
-#if [ ! -d ./gameplay/$1 ]; then
-#	mkdir -p ./gameplay/$1		# doubly check if the folder exists
-#fi
+if [ $# -eq 1 ]; then
+	fps=45
+else
+	fps=$2
+fi
 
-rm -r gameplay
-rm -r event_logs
-
-java -cp target/Mario-1.0-jar-with-dependencies.jar com.mojang.mario.FrameLauncher -gid $1 -fps $2 2>&1 
+java -cp target/Mario-1.0-jar-with-dependencies.jar \
+	com.mojang.mario.FrameLauncher \
+	-gid $1 -fps ${fps}
